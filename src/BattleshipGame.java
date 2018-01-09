bd4a7275a88d67e6a0bfd601cfffc6a3edb99d12
Zac <24833352@users.noreply.github.com>
2018-01-08 23:08:07 -0500

    Added a test case, to test a specific part

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -6,11 +6,29 @@ public class BattleshipGame
 	private BattleshipPlayer p;
 	private BattleshipGrid g = new BattleshipGrid();
 	//creates an Ocean and initializes the number of turns to zero. It also calls the startGame method for the player
+	/*
+	 * "Aircraft Carrier"; Length: 5
+		"Battleship"; Length: 4
+		"Cruiser"; Length: 3 light blue
+		"Submarine"; Length: 3
+		"Destroyer"; Length: 2
+	 */
 	public BattleshipGame(BattleshipPlayer player) throws Exception
 	{
 		p = player;
 		p.setGrid(g);
-		oc.placeAllBoats();
+		//oc.placeAllBoats();
+		///////////////////////////////////////// TEST CASE 1/////////////////////////////////////////////////
+		Position ac1 = new Position (0,2);
+		oc.placeBoat("Aircraft Carrier", "horizontal", ac1);
+		Position bs1 = new Position (6,9);
+		oc.placeBoat("Battleship", "vertical", bs1);
+		Position cs1 = new Position (3,0);
+		oc.placeBoat("Cruiser", "vertical", cs1);
+		Position sub1 = new Position (9,1);
+		oc.placeBoat("Submarine", "horizontal", sub1);
+		Position des1 = new Position (3,9);
+		oc.placeBoat("Destroyer", "vertical", des1);
 		//oc.dumpGrid();		// FOR TESTING ONLY
 		player.startGame();
 	}
@@ -64,8 +82,8 @@ public class BattleshipGame
 
 			p.updatePlayer(pos,  hit,  initial,  boatName,  sunk,  gameOver,  tooManyTurns, turns);
 		}
-		
-		
+
+
 		return turns;
 	}
 }
